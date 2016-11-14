e7cc15c0920a315d98fe8c3560d9f3ec90e8b956
Lucas M <lucas.mull@hotmail.fr>
2016-11-14 10:27:19 +0100

    Lancement de la scène du simon lorsqu'on clique sur le bouton correspondant

--- Assets/Scripts/LoadLevel.cs
+++ Assets/Scripts/LoadLevel.cs
@@ -1,15 +1,12 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 using System.Collections;
 
 public class LoadLevel : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public void LoadGame(int gameId)
+    {
+        SceneManager.LoadScene(gameId);
+    }
+
 }
