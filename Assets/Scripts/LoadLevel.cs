5f891703a30c0d35bfc0740599a1faa7a00284f0
Lucas M <lucas.mull@hotmail.fr>
2016-11-23 11:31:50 +0100

    Menu fonctionnel et mène vers les 4 scènes des jeux

--- Assets/Scripts/LoadLevel.cs
+++ Assets/Scripts/LoadLevel.cs
@@ -1,15 +1,24 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 using System.Collections;
 
 public class LoadLevel : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+    public GameObject MenuCanvas;
+
+    // Use this for initialization
+    void Start () {
+	    
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+    public void LoadLevelOnClick(int gameId)
+    {
+        MenuCanvas.SetActive(false);
+        //SceneManager.LoadScene(gameId);
+    }
 }
