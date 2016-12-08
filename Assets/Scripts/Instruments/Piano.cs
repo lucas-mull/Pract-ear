ffdd5e3697d9d469217879f597e4ec09cf12accd
Lucas M <lucas.mull@hotmail.fr>
2016-12-08 13:46:15 +0100

    Création scène de base pour le Simon. Génération et placement des 4 instruments. Jouent un son lorsqu'on clique dessus.

---
+++ Assets/Scripts/Instruments/Piano.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Piano : Instrument {
+
+    public Piano() : base(Instrument.PIANO) { }
+
+    public override Vector3 getFarLeftVector()
+    {
+        return new Vector3(14, 64, 43);
+    }
+
+    public override Vector3 getFarRightVector()
+    {
+        return new Vector3(115, 64, 43);
+    }
+
+    public override Vector3 getMiddleLeftVector()
+    {
+        return new Vector3(29, 64, 150);
+    }
+
+    public override Vector3 getMiddleRightVector()
+    {
+        return new Vector3(79, 64, 150);
+    }
+}
---
+++ Assets/Scripts/Instruments/Trompette.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Trompette : Instrument
+{
+    public Trompette() : base(Instrument.TROMPETTE) { }
+
+    public override Vector3 getFarLeftVector()
+    {
+        return new Vector3(95, 82, -50);
+    }
+
+    public override Vector3 getFarRightVector()
+    {        
+        return new Vector3(195, 82, -50);
+    }
+
+    public override Vector3 getMiddleLeftVector()
+    {
+        return new Vector3(110, 82, 9);
+    }
+
+    public override Vector3 getMiddleRightVector()
+    {
+        return new Vector3(180, 82, 9);
+    }
+}
