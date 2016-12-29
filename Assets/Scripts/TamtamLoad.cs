51fa58f4f97fa5961064ec738ea0cff348f2dd10
ThibaultCharr <thibault.charriere@gmail.com>
2016-12-29 12:06:53 +0100

    bug

--- Assets/Scripts/TamtamLoad.cs
+++ Assets/Scripts/TamtamLoad.cs
@@ -1,10 +1,9 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
-using System;
 using System.Collections;
+using System.Collections.Generic;
 
-public class TamTamLoad : MonoBehaviour {
-
+public class TamtamLoad : MonoBehaviour {
 
     const string DIFFICULTY_EASY = "easy";
     const string DIFFICULTY_MEDIUM = "medium";
@@ -15,36 +14,165 @@ public class TamTamLoad : MonoBehaviour {
 
     #region Attributs assignés à travers l'Inspector
 
-    public Button _play1stButton;           // Bouton 'Play' jouer / arreter l'extrait du premier tamtam
-    public Button _play2ndButton;           // Bouton 'Play' jouer / arreter l'extrait du deuxieme tamtam
-    public Button _play3rdButton;           // Bouton 'Play' jouer / arreter l'extrait du troisieme tamtam
-    public Button _play4thButton;           // Bouton 'Play' jouer / arreter l'extrait du quatrieme tamtam
-    public Canvas _igInterface;             // Canvas contenant l'interface du jeu
-    public Sprite _playSprite;              // Sprite 'Play' du bouton play
-    public Sprite _pauseSprite;             // Sprite  'Pause' du bouton play
-    public Camera _mainCamera;              // Camera de la scene
-    public Text _questionText;              // Texte qui afiche la question actuelle
+       
+    public Camera _mainCamera;                  // Camera de la scène
 
-    // Ensemble des spots en fonction de leur position sur la scene
-    public Light farLeftLight;
-    public Light farRightLight;
-    public Light middleLeftLight;
-    public Light middleRightLight;
-
+    public Light farLeftLight;                  // Spot en bas à gauche
+    public Light farRightLight;                 // Spot en bas à droite
+    public Light middleLeftLight;               // Spot en haut à gauche
+    public Light middleRightLight;              // Spot en haut à droite
+    
     #endregion
 
-    #region Attributs privés
+    #region Attributs privées
 
-    string difficulty_level;                //niveau de difficulté
+    string difficulty_level;                    // Niveau de difficulté
+
+    List<TamtamInstrument> _instruments;
+    TamtamQuestion _question;
+
+    AudioSource _extractAudioSource;            // Source audio pour l'extrait
+    AudioSource _sfxAudioSource;                // Source audio pour les sound effects
+
+    AudioClip _clipSpotlight;                   // Clip du projecteur qui s'allume / s'éteint
+    AudioClip _clipApplause;                    // Clip des gens qui applaudissent
+    AudioClip _clipBoo;                         // Clip des gens qui huent
+
+
+    #endregion
 
 
     // Use this for initialization
     void Start () {
+        SetDifficulty(DIFFICULTY_EASY);
+
+        _sfxAudioSource = GetComponent<AudioSource>();
+
+        _clipSpotlight = Resources.Load<AudioClip>("SFX/spotlight_on");
+        _clipApplause = Resources.Load<AudioClip>("SFX/crowd_applause");
+        _clipBoo = Resources.Load<AudioClip>("SFX/crowd_boo");
+
+        _question = new TamtamQuestion();
+       
+
+        PlaceInstruments();
+    }
 	
-	}
-    #endregion
-    // Update is called once per frame
-    void Update () {
-	
-	}
+	// Update is called once per frame
+	void Update () {
+        // Détection d'un click sur Android ou PC.
+        bool clicked;
+        bool particle = true;
+        Vector3 clickedPosition = new Vector3();
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            clicked = Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began;
+            if (clicked)
+                clickedPosition = Input.GetTouch(0).position;
+        }
+        else
+        {
+            clicked = Input.GetMouseButtonDown(0);
+            if (clicked)
+                clickedPosition = Input.mousePosition;
+        }
+
+        // Si un clic a été réalisé
+        if (clicked)
+        {
+            Ray ray = _mainCamera.ScreenPointToRay(clickedPosition);
+            RaycastHit hit;
+
+            // On cherche quel instrument a été cliqué
+            if (Physics.Raycast(ray, out hit))
+            {
+                for (int i = 0; i < _instruments.Count ; i++)
+                {
+                    _instruments[i].Instrument.EnableParticles(false);
+
+                    if (hit.transform.name == _instruments[i].Instrument.Name )
+                    {
+                        _instruments[i].Instrument.EnableParticles(true);
+                        
+                        // Et on toggle le spotlight correspondant (on ou off en fonction de ce qu'il était avant)
+                        _instruments[i].ToggleLight();
+                        _sfxAudioSource.PlayOneShot(_clipSpotlight, 0.5f);
+                    }
+                }
+            }
+        }
+    }
+
+    public void Validate()
+    {
+        bool success = true;
+        foreach (TamtamInstrument instrument in _instruments)
+        {
+            if (!instrument.ToggleLightAnswerFor(true))
+            {
+                success = false;
+            }
+        }
+
+        if (success)
+        {
+            _sfxAudioSource.PlayOneShot(_clipApplause, 0.8f);
+        }
+        else
+        {
+            _sfxAudioSource.PlayOneShot(_clipBoo, 0.8f);
+        }
+
+    }
+
+    void SetDifficulty(string difficulty)
+    {
+        switch (difficulty)
+        {
+            case DIFFICULTY_EASY:
+            case DIFFICULTY_MEDIUM:
+            case DIFFICULTY_HARD:
+                this.difficulty_level = difficulty;
+                break;
+            default:
+               // Console.WriteLine(difficulty + " is not a valid difficulty input");
+                break;
+        }
+    }
+
+
+    void PlaceInstruments()
+    {
+        _instruments = new List<TamtamInstrument>();
+        _instruments.Add(new TamtamInstrument("tamtam", false, farLeftLight));
+        _instruments.Add(new TamtamInstrument("tamtam", false, farRightLight));
+        _instruments.Add(new TamtamInstrument("tamtam", true, middleLeftLight));
+        _instruments.Add(new TamtamInstrument("tamtam", false, middleRightLight));
+
+
+        // Far left
+        _instruments[0].Instrument.PutFarLeft(null);
+        _instruments[0].SpotLight = farLeftLight;
+        _instruments[0].Instrument.Name = "tamtam1";
+
+
+        // Far right
+        _instruments[1].Instrument.PutFarRight(null);
+        _instruments[1].SpotLight = farRightLight;
+        _instruments[0].Instrument.Name = "tamtam2";
+
+
+        // Middle Left
+        _instruments[2].Instrument.PutMiddleLeft(null);
+        _instruments[2].SpotLight = middleLeftLight;
+        _instruments[0].Instrument.Name = "tamtam3";
+
+
+        // Middle Right
+        _instruments[3].Instrument.PutMiddleRight(null);
+        _instruments[3].SpotLight = middleRightLight;
+        _instruments[0].Instrument.Name = "tamtam4";
+
+    }
+
 }
