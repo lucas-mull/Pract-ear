d5e2ba57a413bb6a69bd47b9abbc608fd2ea0f3c
ThibaultCharr <thibault.charriere@gmail.com>
2016-12-29 11:22:32 +0100

    recup du travail de valentin

---
+++ Assets/Scripts/Instruments/TamtamInstrument.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public class TamtamInstrument : BlindTestInstrument {
+
+    // attribut indiquant si le tamtam est l'intru
+    public bool isIntru = false;
+
+    public TamtamInstrument(string instrumentName) :base(instrumentName)
+    {
+    }
+	
+    public TamtamInstrument(string instrumentName, bool isIntru) : this(instrumentName)
+    {
+        this.isIntru = isIntru;
+    }
+
+    public TamtamInstrument(string instrumentName, bool isIntru, Light spotlight) : this(instrumentName, isIntru)
+    {
+        this.SpotLight = spotlight;
+    }
+
+    /// <summary>
+    /// Méthode dérivée de la classe BlindTestInstrument
+    /// </summary>
+    /// <param name="affirmative"></param>
+    /// <returns></returns>
+
+    public new bool ToggleLightAnswerFor(bool affirmative)
+    {
+        if (isIntru && this.isLit)
+        {
+            this.SpotLight.color = Color.green;
+            return true;
+        }
+        else if (!isIntru && this.isLit)
+        {
+            this.SpotLight.color = Color.red;
+            return false;
+        }
+        else if (isIntru && !this.isLit)
+        {
+            this.ToggleLight();
+            this.SpotLight.color = Color.green;
+            return false;
+        }
+        else
+            return true;
+    }
+}
