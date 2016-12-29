cbf6332676943dea79337e219e07d345fd23e122
ThibaultCharr <thibault.charriere@gmail.com>
2016-12-29 11:54:50 +0100

    bug

--- Assets/Scripts/Instruments/TamtamIntruInstrument.cs
+++ Assets/Scripts/Instruments/TamtamIntruInstrument.cs
@@ -1,6 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+/// <summary>
+/// Classe utilisée pour les instruments du jeu TamTam Intru
+/// Essentiellement rajoute une spotlight associé à l'instrument pour que ce soit plus facile à toggle on / off.
+/// </summary>
 public class TamtamIntruInstrument : MonoBehaviour {
 
 	// Use this for initialization
