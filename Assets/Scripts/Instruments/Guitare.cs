b47a98c70f5f8fe56c04688e2ce3551153bf577d
Lucas M <lucas.mull@hotmail.fr>
2017-01-18 17:49:32 +0100

    Ajout des nouveaux instruments (manque plus que les sons)

---
+++ Assets/Scripts/Instruments/Guitare.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Guitare : Instrument
+{
+    public Guitare() : base(Instrument.GUITARE,
+        Category.GetInstance(Category.PINCEES))
+    { }
+
+    public override Vector3 getFarLeftVector()
+    {
+        return new Vector3(20, 40, 130);
+    }
+
+    public override Vector3 getFarRightVector()
+    {
+        return new Vector3(85, 40, 130);
+    }
+
+    public override Vector3 getMiddleLeftVector()
+    {
+        return new Vector3(30, 40, 190);
+    }
+
+    public override Vector3 getMiddleRightVector()
+    {
+        return new Vector3(75, 40, 190);
+    }
+}
