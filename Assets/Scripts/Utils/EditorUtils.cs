039db47d8a4f7a232b289e214d4723336e4a2d90
MSI\lucas <lucas.mull@hotmail.fr>
2017-12-27 21:47:50 +0100

    Added a generic Audio Playback tool that can be reused in multiple scenes (blind test for examples). Added a few more helper methods, most notably for the editor. Changed target platform to Android, changed default project settings to 2D. Reimported broken shader from TextMeshPro

--- Assets/Scripts/Utils/EditorUtils.cs
+++ Assets/Scripts/Utils/EditorUtils.cs
@@ -2,26 +2,83 @@
 
 using UnityEngine;
 using UnityEditor;
+using System;
+using Object = UnityEngine.Object;
 
-static public class EditorUtils
+namespace Practear.Utils
 {
-
-    #region Static methods
-
-    /// <summary>
-    /// Draw an error message using a red label field.
-    /// </summary>
-    /// <param name="message">The message to display</param>
-    static public void DrawErrorMessage(string message)
+    static public class EditorUtils
     {
-        GUIStyle style = new GUIStyle(EditorStyles.boldLabel);
-        style.normal.textColor = Color.red;
 
-        EditorGUILayout.LabelField(message, style);
+        #region Static methods
+
+        /// <summary>
+        /// Draw the default "script" field that shows in the inspector for every monobehaviour.
+        /// </summary>
+        /// <typeparam name="T">The type of the script</typeparam>
+        /// <param name="target">The target</param>
+        static public void DrawDefaultScriptField<T>(Object target) where T : MonoBehaviour
+        {
+            MonoScript script = MonoScript.FromMonoBehaviour((T)target);
+            EditorGUI.BeginDisabledGroup(true);
+            EditorGUILayout.ObjectField("Script", script, typeof(MonoScript), false);
+            EditorGUI.EndDisabledGroup();
+        }
+
+        /// <summary>
+        /// Draw an error message using a red label field.
+        /// </summary>
+        /// <param name="message">The message to display</param>
+        static public void DrawErrorMessage(string message)
+        {
+            GUIStyle style = new GUIStyle(EditorStyles.boldLabel);
+            style.normal.textColor = Color.red;
+
+            EditorGUILayout.LabelField(message, style);
+        }
+
+        /// <summary>
+        /// Draw property field(s)
+        /// </summary>
+        /// <param name="serializedObject">The serialized object</param>
+        /// <param name="propertyName">The property's name</param>
+        /// <param name="additionalProperties">Additional properties names</param>
+        static public void DrawPropertyFields(SerializedObject serializedObject, string propertyName, params string[] additionalProperties)
+        {
+            SerializedProperty initial = serializedObject.FindProperty(propertyName);
+            if (initial == null)
+                throw new ArgumentException(string.Format("Couldn't find property '{0}'.", propertyName));
+
+            EditorGUILayout.PropertyField(initial, true);
+
+            foreach (string additionalProperty in additionalProperties)
+            {
+                SerializedProperty additional = serializedObject.FindProperty(additionalProperty);
+                if (additional == null)
+                    throw new ArgumentException(string.Format("Couldn't find property '{0}'.", additionalProperty));
+
+                EditorGUILayout.PropertyField(additional, true);
+            }
+        }
+
+        /// <summary>
+        /// Draw a image field using the standard object reference field rather than the sprite field.
+        /// </summary>
+        /// <param name="label">The label to display</param>
+        /// <param name="sourceImage">The sprite.</param>
+        static public Sprite DrawNormalSpriteField(string label, Sprite sourceImage, bool allowSceneObjects = false)
+        {
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.PrefixLabel(label);
+            sourceImage = (Sprite)EditorGUILayout.ObjectField(sourceImage, typeof(Sprite), allowSceneObjects);
+            EditorGUILayout.EndHorizontal();
+
+            return sourceImage;
+        }
+
+        #endregion // Static methods
+
     }
-
-    #endregion // Static methods
-
 }
 
 #endif
---
+++ Assets/Scripts/Utils/PropertyAttributes/EnableIfAttribute.cs
@@ -0,0 +1,136 @@
+﻿using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace Practear.Utils.PropertyAttributes
+{
+    /// <summary>
+    /// Use this attribute to enable a serialized field in the inspector depending of the value of
+    /// another property.
+    /// </summary>
+    sealed public class EnableIfAttribute : PropertyAttribute
+    {
+
+        #region Instance variables
+
+        /// <summary>
+        /// The name of the property to depend on.
+        /// Has to be located within the same script.
+        /// </summary>
+        private string m_ConditionPropertyName;
+
+        #endregion // Instance variables
+
+        #region Properties
+
+        /// <summary>
+        /// <see cref="m_ConditionPropertyName"/>
+        /// </summary>
+        public string ConditionPropertyName { get { return m_ConditionPropertyName; } }
+
+        /// <summary>
+        /// If this is set to true, disabled fields will be hidden altogether.
+        /// </summary>
+        public bool HideIfDisabled { get; set; }
+
+        #endregion // Properties
+
+        #region Constructors
+
+        /// <summary>
+        /// Basic constructor
+        /// </summary>
+        /// <param name="propertyName">The name of the conditional property.</param>
+        public EnableIfAttribute(string propertyName)
+        {
+            m_ConditionPropertyName = propertyName;
+        }
+
+        #endregion // Constructors
+
+    }
+
+#if UNITY_EDITOR
+
+    /// <summary>
+    /// Property drawer for <see cref="EnableIfAttribute"/>
+    /// </summary>
+    [CustomPropertyDrawer(typeof(EnableIfAttribute))]
+    public class EnableIfDrawer : PropertyDrawer
+    {
+
+        #region Properties
+
+        /// <summary>
+        /// Access the attribute.
+        /// </summary>
+        private EnableIfAttribute Target { get { return (EnableIfAttribute)attribute; } }
+
+        #endregion // Properties
+
+        #region Methods
+
+        /// <summary>
+        /// <see cref="PropertyDrawer.GetPropertyHeight"/>
+        /// </summary>
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            if (!GetConditionalProperty(property.serializedObject) && Target.HideIfDisabled)
+            {
+                return 0f;
+            }
+            else
+            {
+                return base.GetPropertyHeight(property, label);
+            }
+        }
+
+        /// <summary>
+        /// <see cref="PropertyDrawer.OnGUI"/>
+        /// </summary>
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            if (GetConditionalProperty(property.serializedObject))
+            {
+                EditorGUI.PropertyField(position, property, label);
+            }
+            else if (!Target.HideIfDisabled)
+            {
+                EditorGUI.BeginDisabledGroup(true);
+                EditorGUI.PropertyField(position, property, label);
+                EditorGUI.EndDisabledGroup();
+            }
+        }
+
+        /// <summary>
+        /// Access the conditional serialized property
+        /// </summary>
+        /// <param name="obj">The serialized object holding the property.</param>
+        /// <returns>The serialized property (null if not found).</returns>
+        private bool GetConditionalProperty(SerializedObject obj)
+        {
+            SerializedProperty prop = obj.FindProperty(Target.ConditionPropertyName);
+            if (prop == null)
+                return false;
+
+            switch (prop.propertyType)
+            {
+                case SerializedPropertyType.Boolean:
+                    return prop.boolValue;
+                case SerializedPropertyType.ObjectReference:
+                    return prop.objectReferenceValue != null;
+                default:
+                    return false;
+            }
+        }
+
+        #endregion // Methods
+
+    }
+
+#endif
+
+}
+
